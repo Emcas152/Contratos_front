b1d534b1ee5c481b274a4b295956aa9312ef1bae
DESKTOP-N4Q6HSI\Alew <desarrollofrontend@swarmdesarrollo.com>
2024-07-24 11:36:21 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit,ViewChild  } from '@angular/core';
 import { MatHorizontalStepper } from '@angular/material/stepper';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-
+import { FormControl } from '@angular/forms';
 import Swal from 'sweetalert2';
 const opcionesPorTipo = [ 
    
@@ -47,13 +47,15 @@ const opcionesPorTipo = [
     tipo_proyecto: 'VILLA LUZ',
     torres: [
       { value: '1', label: 'Torre 1' },
-      { value: '2', label: 'Torre 2' },
-      { value: '3', label: 'Torre 3' },
-      { value: '4', label: 'Torre 4' },
-      { value: '5', label: 'Torre 5' },
-      { value: '6', label: 'Torre 6' },
-      { value: '7', label: 'Torre 7' },
-      { value: '8', label: 'Torre 8' }
+      { value: '2', label: 'Torre 2' }
+    ]
+  },
+  {
+
+    tipo_proyecto: 'GEMINIS 10',
+    torres: [
+      { value: '1', label: 'Torre Norte' },
+      { value: '2', label: 'Torre Sur' }
     ]
   },
   {
@@ -79,7 +81,57 @@ const opcionesPorTipo = [
 })
 
 export class AppComponent implements OnInit {
+
+  selectedOption: string = '';
+  selectedRadioButton: string = '';
   
+  // FormControl for each radio group
+
+
+
+  onRadioGroupChange(option: string,valor:string, tabla:string): void {
+    this.formServicio.get('tipo_servicio')?.setValue(option);
+    this.formServicio.get(tabla)?.setValue(valor);
+
+
+    if (tabla === 'tabla0') {
+      this.formServicio.get('tabla1')?.setValue(0);
+      this.formServicio.get('tabla2')?.setValue(0);
+    }
+  
+    if (tabla === 'tabla1') {
+      this.formServicio.get('tabla0')?.setValue(0);
+      this.formServicio.get('tabla2')?.setValue(0);
+    }
+  
+    if (tabla === 'tabla2') {
+      this.formServicio.get('tabla0')?.setValue(0);
+      this.formServicio.get('tabla1')?.setValue(0);
+    }
+    console.log(this.formServicio.get('tabla1')?.value);
+    console.log(this.formServicio.get('tabla2')?.value);
+    console.log(this.formServicio.get('tabla0')?.value);
+    console.log(this.formServicio.get('tipo_servicio')?.value);
+  }
+
+   onRadioChange(option: string): void {
+    if (this.selectedRadioButton !== '' && this.selectedRadioButton !== option) {
+      this.selectedRadioButton = ''; // Restablece la selección previa
+      this.selectedOption = ''; // Desbloquea la opción previa
+
+      // Resetear los controles de los radio buttons
+      this.formServicio.get('tabla0')?.reset();
+      this.formServicio.get('tabla1')?.reset();
+      this.formServicio.get('tabla2')?.reset();
+    }
+    this.selectedRadioButton = option; // Establece la nueva selección
+    this.selectedOption = option;
+    console.log('Form value after change:', this.formServicio.get('tipo_servicio')?.value);
+    console.log('Form value after change:', this.formServicio.get('tabla0')?.value);
+    console.log('Form value after change:', this.formServicio.get('tabla1')?.value);
+    console.log('Form value after change:', this.formServicio.get('tabla2')?.value);
+  } 
+
   @ViewChild(MatHorizontalStepper) stepper!: MatHorizontalStepper;
   selected = 'option2';
   selectedTipo: string = '';
@@ -87,6 +139,8 @@ export class AppComponent implements OnInit {
   selectedValue: string = '';
   isRadioSelected: boolean = false;
   firstFormGroup: FormGroup ;
+
+  showEnergyInfo = false;
 
   thirdFormGroup: FormGroup = Object.create(null);
   fourthFormGroup: FormGroup = Object.create(null);
@@ -121,7 +175,9 @@ export class AppComponent implements OnInit {
   isLoading: boolean = false;
   torreSelect = false;
   selectedTower: string = '';
-  
+  isFormCompleted = false;
+
+
   steps: Array<{label: string, completed: boolean}> = [
     {label: 'Paso 1', completed: false},
     {label: 'Paso 2', completed: false},
@@ -141,8 +197,10 @@ export class AppComponent implements OnInit {
   nextStep() {
      this.selectedValue = this.firstFormGroup.get('tipo_proyecto')?.value;
      this.selectedTipo = this.thirdFormGroup.get('tipo_apartamento')?.value;
+
+   
     
-    if (this.selectedValue === 'VIAGGIO') {
+    if (this.selectedValue === 'VIAGGIO' ) {
       this.aguaIcon = "cancel";
       this.aguaIconColor = '#98989a';
      // this.stepper.selectedIndex = 1;
@@ -151,15 +209,22 @@ export class AppComponent implements OnInit {
       this.secondFormGroup.patchValue({
         torre: 'UNICA'
       });
-      
-      
-     
         this.stepper.next();
  
       
     } else {
-      this.aguaIcon = "task_alt";
-      this.aguaIconColor = '#035C67';
+      if(this.selectedValue === 'VILLA LUZ'){
+        this.aguaIconColor = '#98989a';
+        this.aguaIcon = "cancel";
+      }
+      else{
+        this.aguaIconColor = '#035C67';
+        this.aguaIcon = "task_alt";
+      }
+
+
+      
+     
       this.torreSelect = false;
       setTimeout(() => {
         this.stepper.next();
@@ -202,10 +267,24 @@ isFormValid(): boolean {
     this.fourthFormGroup.get('agua')?.value
   );
 }
+
+isServiceSelected(): boolean {
+  const { agua, energia, internet } = this.fourthFormGroup.value;
+  return agua || energia || internet;
+}
+
 public secondFormGroup = this._formBuilder.group({
   torre: ['', Validators.required],
   numero_apartamento: ['', [Validators.required]],
 });
+
+isBuildingSelected(): boolean {
+  return this.firstFormGroup.get('tipo_proyecto')?.value !== '';
+}
+
+isContractTypeSelected(): boolean {
+  return this.thirdFormGroup.get('tipo_apartamento')?.value !== '';
+}
 
 
 public formSolicitante: FormGroup = this._formBuilder.group({
@@ -230,9 +309,6 @@ public formSolicitante: FormGroup = this._formBuilder.group({
 public formFacturacion: FormGroup = this._formBuilder.group({
   nombre_factura: [''],
   nit_factura: [''],
-
-
- 
 });
   
 isSolicitanteFormCompleted(): boolean {
@@ -272,6 +348,10 @@ isSolicitanteFormCompleted(): boolean {
       internet: [''] 
     });
 
+    this.fourthFormGroup.get('energia')?.valueChanges.subscribe(value => {
+      this.showEnergyInfo = value;
+    });
+
     this.fourthFormGroup.valueChanges.subscribe((values) => {
       if (values.internet && !values.agua && !values.energia) {
         this.selectedPlans = 'PLAN1';
@@ -294,13 +374,14 @@ isSolicitanteFormCompleted(): boolean {
     });
 
 
+   
+
 
    
     this.formFinal = this._formBuilder.group({
       termsAccepted: [false, Validators.requiredTrue],
     });
-    
-    
+
   }
   public formProyecto: FormGroup = this._formBuilder.group({
     tipo_proyecto: [[Validators.required]],
@@ -429,6 +510,8 @@ isSolicitanteFormCompleted(): boolean {
     console.log(this.firstFormGroup.get('tipo_proyecto')?.value);
     console.log(this.thirdFormGroup.get('tipo_apartamento')?.value);
     console.log(this.formServicioAgua.get('servicio_agua')?.value);
+    console.log(this.formServicio.get('tabla1')?.value);
+    console.log(this.formServicio.get('tipo_servicio')?.value);
 
     let info = {
       datos: this.formSolicitante.value,
@@ -484,6 +567,17 @@ isSolicitanteFormCompleted(): boolean {
 
   downloadPdfEnergia() {
     window.open(this.pdfUrlEnergia, '_blank');
+  }
+
+
+  onBuildingSelectionChange(event: any) {
+    if (event.value === 'VILLA LUZ') {
+      // Avanzar al siguiente paso y omitir el paso de "Tipo de Contratante"
+      this.stepper.next(); // Avanza al siguiente paso (si hay un paso intermedio)
+      this.stepper.selectedIndex = 3; // Configura el índice del paso al paso de servicios (asume que es el paso 3)
+    } else {
+      this.stepper.next(); // Avanza al siguiente paso para otros valores
+    }
   }
 
 }
